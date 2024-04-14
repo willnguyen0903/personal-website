4a547fc26b21b5aebe9bef199c7f4c7d862eedde
willnguyen0903 <trnguyen0903@gmail.com>
2024-04-14 16:20:53 -0400

    Initial commit

---
+++ subpage_about/script.js
@@ -0,0 +1,20 @@
+
+//JavaScript to increase font size and make font bold when "HOME" link is clicked
+document.getElementById("home-link").addEventListener("click", function() {
+  // Increase font size and make font bold
+  this.style.fontSize = "1.2em";
+  this.style.fontWeight = "bold";
+  });
+document.getElementById("about-link").addEventListener("click", function() {
+  this.style.fontSize = "1.2em";
+  this.style.fontWeight = "bold";
+  });
+document.getElementById("project-link").addEventListener("click", function() {
+  this.style.fontSize = "1.2em";
+  this.style.fontWeight = "bold";
+  });
+  
+document.getElementById("contact-link").addEventListener("click", function() {
+  this.style.fontSize = "1.2em";
+  this.style.fontWeight = "bold";
+  });  
